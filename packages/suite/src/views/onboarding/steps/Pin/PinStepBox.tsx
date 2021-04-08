a8f934077dfc7f652d58515a3c0a36dd62063843
slowbackspace <slowbackspace@gmail.com>
2021-04-08 20:41:36 +0200

    feat(onboarding): pin step

---
+++ packages/suite/src/views/onboarding/steps/Pin/PinStepBox.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { OnboardingStepBox, OnboardingStepBoxProps } from '@firmware-components';
+
+const PinStepBox = (props: OnboardingStepBoxProps) => {
+    return <OnboardingStepBox image="PIN" {...props} />;
+};
+
+export default PinStepBox;
